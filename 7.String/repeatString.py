313fb67845de84bef247d634172ee31c50e7e066
Dodti <doradoralee@naver.com>
2019-03-29 18:34:35 +0900

    #2675

--- 7.String/repeatString.py
+++ 7.String/repeatString.py
@@ -1,6 +1,16 @@
-string = input()
-alphabet = []
+num = int(input())
+count = []
+string = []
+for i in range(0, num):
+    string.append(input())
 
-for i in range(97, 123):
-    alphabet.append(string.find(chr(i)))
-print(alphabet)
+for i in range(0, num):
+    tmp1, tmp2 = string[i].split(' ')
+    count.append(int(tmp1))
+    string[i] = tmp2
+
+for i in range(0, num):
+    for j in string[i]:
+        for k in range(0, count[i]):
+            print(j, end='')
+    print()
