22f336b7c6d754c96e584d0d6fa611f6354b4010
changbambi <doradoralee@naver.com>
2019-05-10 23:03:29 +0900

    #1193

--- 7.String/Sangsu.cpp
+++ 7.String/Sangsu.cpp
@@ -3,20 +3,21 @@
 #include <algorithm>
 using namespace std;
 
-int main(void){
+int main(void)
+{
     string str1, str2;
     int num1, num2, max_num;
 
-    cin>>str1>>str2;
+    cin >> str1 >> str2;
 
     reverse(str1.begin(), str1.end());
     reverse(str2.begin(), str2.end());
 
     num1 = atoi(str1.c_str());
     num2 = atoi(str2.c_str());
-    max_num = max(num1,num2);
+    max_num = max(num1, num2);
 
-    cout<<max_num<<endl;
+    cout << max_num << endl;
 
     return 0;
 }
