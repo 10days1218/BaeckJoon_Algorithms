fb280ba0b2fdc7b88739487030b1c7f590b642d6
changbambi <doradoralee@naver.com>
2019-05-15 23:10:57 +0900

    #1011

--- 8.Rule/Fly_me_to_the_Alpha_centauri.cpp
+++ 8.Rule/Fly_me_to_the_Alpha_centauri.cpp
@@ -4,5 +4,14 @@ using namespace std;
 
 int main(void)
 {
+    int num = 0;
+    cin >> num;
+
+    int *x = new int[num];
+    int *y = new int[num];
+
+    for (int i = 0; i < num; i++)
+        cin >> x[i] >> y[i];
+
     return 0;
 }
