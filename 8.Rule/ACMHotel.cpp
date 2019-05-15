6c4ad757742aaa810ecb8f3fbcdf3c9fe22f7310
changbambi <doradoralee@naver.com>
2019-05-15 23:26:05 +0900

    #10250

---
+++ 8.Rule/ACMHotel.cpp
@@ -0,0 +1,8 @@
+//#10250
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    return 0;
+}
--- 8.Rule/Fly_me_to_the_Alpha_centauri.cpp
+++ 8.Rule/Fly_me_to_the_Alpha_centauri.cpp
@@ -6,12 +6,18 @@ int main(void)
 {
     int num = 0;
     cin >> num;
-
+    int count = 0;
     int *x = new int[num];
     int *y = new int[num];
 
     for (int i = 0; i < num; i++)
         cin >> x[i] >> y[i];
 
+    for (int i = 0; i < num; i++)
+    {
+        while (1)
+        {
+        }
+    }
     return 0;
 }
