df86b99aef7b37758b821782f485c0c2fab79ac0
changbambi <doradoralee@naver.com>
2019-05-19 23:33:58 +0900

    #1011

--- 8.Rule/ACMHotel.cpp
+++ 8.Rule/ACMHotel.cpp
@@ -4,5 +4,20 @@ using namespace std;
 
 int main(void)
 {
+    int n = 0;
+    cin >> n;
+    int *H = new int[n];
+    int *W = new int[n];
+    int *N = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> H[i] >> W[i] >> N[n];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        int index1 = H[i];
+        int index2 = W[i];
+    }
     return 0;
 }
