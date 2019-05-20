ad4718941e62dcb77a0d87cb81973386440253e6
changbambi <doradoralee@naver.com>
2019-05-20 14:02:09 +0900

    #1475

--- 8.Rule/RoomNumber.cpp
+++ 8.Rule/RoomNumber.cpp
@@ -1,8 +1,91 @@
 //#1475
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(void)
 {
+    int roomNumber = 0;
+    cin >> roomNumber;
+
+    string str = to_string(roomNumber);
+    int len = str.length();
+
+    int *num = new int[len];
+    int div = 1;
+
+    for (int i = 0; i < len - 1; i++)
+        div *= 10;
+
+    for (int i = 0; i < len; i++)
+    {
+        if (i == len - 1)
+            num[i] = roomNumber % 10;
+
+        else
+        {
+            num[i] = roomNumber / div;
+            roomNumber = roomNumber % div;
+        }
+
+        div /= 10;
+    }
+
+    int number[9] = {0};
+
+    for (int i = 0; i < len; i++)
+    {
+        switch (num[i])
+        {
+        case 0:
+            number[0] += 1;
+            break;
+        case 1:
+            number[1] += 1;
+            break;
+        case 2:
+            number[2] += 1;
+            break;
+        case 3:
+            number[3] += 1;
+            break;
+        case 4:
+            number[4] += 1;
+            break;
+        case 5:
+            number[5] += 1;
+            break;
+        case 6:
+            number[6] += 1;
+            break;
+        case 7:
+            number[7] += 1;
+            break;
+        case 8:
+            number[8] += 1;
+            break;
+        case 9:
+            number[6] += 1;
+            break;
+        }
+    }
+
+    int max_n = 0;
+    int index = 0;
+
+    for (int i = 0; i < 9; i++)
+    {
+        if (i == 6)
+        {
+            if (number[i] % 2 == 1)
+                number[i] = number[i] / 2 + 1;
+            else
+                number[i] = number[i] / 2;
+        }
+
+        max_n = max(max_n, number[i]);
+    }
+
+    cout << max_n << endl;
     return 0;
 }
