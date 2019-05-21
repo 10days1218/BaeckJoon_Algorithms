1397ca2ac54ecf96be2e38f592fc7aa2771fb4d6
changbambi <doradoralee@naver.com>
2019-05-21 18:02:24 +0900

    #6064

--- 8.Rule/kaing.cpp
+++ 8.Rule/kaing.cpp
@@ -1,7 +1,49 @@
 //#6064
-#include <iotream>
+#include <iostream>
 using namespace std;
+
+int lcm(int m, int n);
+
 int main(void)
 {
+    int num = 0;
+    int m, n, x, y;
+    cin >> num;
+
+    for (int i = 0; i < num; i++)
+    {
+        cin >> m >> n >> x >> y;
+        int lcmN = lcm(m, n);
+        while (x != y && x <= lcmN)
+        {
+            if (x < y)
+                x += m;
+            else
+                y += n;
+        }
+
+        if (x != y)
+        {
+            cout << "-1" << endl;
+        }
+        else
+        {
+            cout << x << endl;
+        }
+    }
+
     return 0;
+}
+
+int lcm(int m, int n)
+{
+    int a = m, b = n, tmp;
+    while (b)
+    {
+        tmp = a;
+        a = b;
+        b = tmp % b;
+    }
+
+    return (m * n) / a;
 }
