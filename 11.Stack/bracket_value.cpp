2b0b015c445119b7938932c08dd8b5bfb258350c
changbambi <doradoralee@naver.com>
2019-05-26 01:43:18 +0900

    #2504

--- 11.Stack/bracket_value.cpp
+++ 11.Stack/bracket_value.cpp
@@ -1,8 +1,31 @@
 //#2504
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(void)
 {
-    return 0;
-}
+    stack<string> stack;
+    string str;
+
+    getline(cin, str, '\n');
+    int len = str.length();
+
+    for (int i = 0; i < len; i++)
+    {
+        if (str.at(i) == "(" || str.at(i) == "[")
+            stack.push(str.at(i));
+
+        if (str.at(i) == ")" && str.top() == "(")
+        {
+            stack.pop();
+            stack.push("2");
+        }
+
+        if (str.at(i) == "]" && str.top() == "[")
+        {
+            stack.pop();
+            stack.push("3");
+        }
+        return 0;
+    }
