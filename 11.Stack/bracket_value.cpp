16c71cf82896451d24293832f63c2b3c9574e803
changbambi <doradoralee@naver.com>
2019-05-26 00:08:03 +0900

    #2504

---
+++ 11.Stack/bracket_value.cpp
@@ -0,0 +1,8 @@
+//#2504
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    return 0;
+}
--- 11.Stack/stack_squence.cpp
+++ 11.Stack/stack_squence.cpp
@@ -1,7 +1,17 @@
+//#1874
 #include <iotream>
+#include <stack>
 using namespace std;
 
 int main(void)
 {
+    int num = 0;
+    cin >> num;
+
+    for (int i = 0; i < num; i++)
+    {
+        ;
+    }
+
     return 0;
 }
