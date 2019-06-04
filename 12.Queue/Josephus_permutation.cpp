b7a6a292d3ec92e6c087d6149078bba9b92845bf
changbambi <doradoralee@naver.com>
2019-06-05 01:27:14 +0900

    #11866

--- 12.Queue/Josephus_permutation.cpp
+++ 12.Queue/Josephus_permutation.cpp
@@ -1,8 +1,22 @@
 //#11866
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main(void)
 {
+    int N, M;
+    vector<int> v;
+    cin >> N >> M;
+    int index = 0;
+    v.resize(N);
+
+    for (int i = 0; i < N; i++)
+        v[i] = i + 1;
+
+    while (1)
+    {
+        ;
+    }
     return 0;
 }
