641bc58fab4238639e0aa75457515eb3b31be431
changbambi <doradoralee@naver.com>
2019-06-04 15:09:36 +0900

    #11866

--- 12.Queue/printQueue.cpp
+++ 12.Queue/printQueue.cpp
@@ -1,7 +1,19 @@
 //#1966
 #include <iostream>
+#include <vector>
 using namespace std;
 int main(void)
 {
+    int num = 0;
+    cin >> num;
+    int *N = new int[num];
+    int *M = new int[num];
+    int** arr = new 
+
+    for (int i = 0; i < num; i++)
+    {
+        cin >> N[i] >> M[i];
+        int
+    }
     return 0;
 }
---
+++ 12.Queue/printQueue.py
@@ -0,0 +1,16 @@
+#1966
+
+num = int(input())
+list_n = []
+list_m = []
+queue = []
+
+for i in range(0, num):
+    tmp = input().split(' ')
+    list_n.append(int(tmp[0]))
+    lit_m.append(int(tmp[1]))
+    queue = input().split(' ')
+
+for i in range(0,num):
+    
+
