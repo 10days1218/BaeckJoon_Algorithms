300899e27455f16937f7711277fbb0d01f8dcb8b
changbambi <doradoralee@naver.com>
2019-06-07 15:19:35 +0900

    #1966

--- 12.Queue/printQueue.cpp
+++ 12.Queue/printQueue.cpp
@@ -1,20 +1,17 @@
 //#1966
 #include <iostream>
 #include <queue>
-#include <vector>
 using namespace std;
 int main(void)
 {
     int num = 0;
-    vector<int> N, M;
+    int n, m;
     cin >> num;
-    N.resize(num);
-    M.resize(num);
-    queue<int> *q = new queue[num];
 
     for (int i = 0; i < num; i++)
     {
-        cin >> N[i] >> M[i];
+        ;
     }
+
     return 0;
 }
--- 12.Queue/printQueue.py
+++ 12.Queue/printQueue.py
@@ -10,9 +10,13 @@ for i in range(0, num):
     list_n.append(int(tmp[0]))
     lit_m.append(int(tmp[1]))
     tmp_q = input().split(' ')
-    queue.append(tmp_q)
+    tmp_q = [int(i) for i in tmp_q]
+    queue.append(tmp_q.sort(Ture))
 
 for i in range(0,num):
+    while 
+
+    
     
     
 
