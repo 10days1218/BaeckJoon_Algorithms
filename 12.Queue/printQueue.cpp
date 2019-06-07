a734ec69e77189890afca4f43be4a19c73ea57f8
changbambi <doradoralee@naver.com>
2019-06-07 20:50:22 +0900

    #1966

--- 12.Queue/printQueue.cpp
+++ 12.Queue/printQueue.cpp
@@ -10,7 +10,37 @@ int main(void)
 
     for (int i = 0; i < num; i++)
     {
-        ;
+        cin >> n >> m;
+        queue<pair<int, int>> q;
+        priority_queue<int> pq;
+        int count = 0;
+        for (int j = 0; j < n; j++)
+        {
+            int tmp;
+            cin >> tmp;
+            q.push({j, tmp});
+            pq.push(tmp);
+        }
+        whie(!q.empty())
+        {
+            int idx = q.front().first();
+            int val = q.front().second();
+            q.pop();
+            if (pq.top() == val)
+            {
+                pq.pop();
+                count++;
+                if (idx == m)
+                {
+                    cout << count << endl;
+                    break;
+                }
+            }
+            else
+            {
+                q.push(idx, val);
+            }
+        }
     }
 
     return 0;
