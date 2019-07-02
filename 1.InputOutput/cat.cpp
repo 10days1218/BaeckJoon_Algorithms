cc7f4f0d85b114a4cabdfc373e50cfc079ee5837
changbambi <doradoralee@naver.com>
2019-07-02 23:23:27 +0900

    #10171

---
+++ 1.InputOutput/cat.cpp
@@ -0,0 +1,13 @@
+//10171
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    cout << "\\    /\\" << endl;
+    cout << " )  ( ')" << endl;
+    cout << "(  /  )" << endl;
+    cout << " \\(__)|" << endl;
+
+    return 0;
+}
