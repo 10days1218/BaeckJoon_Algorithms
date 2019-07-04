f3c3146a8ac2764906cb34315fb5f720e85856cd
changbambi <doradoralee@naver.com>
2019-07-05 00:30:03 +0900

    change files

---
+++ 11.Sort/insertion.cpp
@@ -0,0 +1,23 @@
+//#2750
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(void)
+{
+    int num = 0;
+    vector<int> v;
+    int i_min = 0;
+    cin >> num;
+    v.resize(num);
+
+    for (int i = 0; i < num; i++)
+    {
+        cin >> v[i];
+    }
+
+    for (int i = 0; i < num - 1; i++)
+    {
+        i_min = 0;
+        for (int)
+    }
+}
