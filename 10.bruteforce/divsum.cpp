ccaa574b07cf5fc1cd035b2c2f0ffe300249159c
changbambi <doradoralee@naver.com>
2019-07-13 20:46:10 +0900

    #2231

--- 10.bruteforce/divsum.cpp
+++ 10.bruteforce/divsum.cpp
@@ -4,5 +4,29 @@ using namespace std;
 
 int main(void)
 {
+    int num;
+    int answer = 0;
+    cin >> num;
+
+    for (int i = 1; i < num; i++)
+    {
+        answer += i;
+        int tmp = i;
+        while (tmp != 0)
+        {
+            answer += tmp % 10;
+            tmp /= 10;
+        }
+
+        if (answer == num)
+        {
+            answer = i;
+            break;
+        }
+
+        answer = 0;
+    }
+
+    cout << answer << '\n';
     return 0;
 }
