ddbcafc4a03d505afb7dbe4ffc57b9b321bef7ad
changbambi <doradoralee@naver.com>
2019-07-15 17:49:31 +0900

    #1018

--- 10.bruteforce/chese.cpp
+++ 10.bruteforce/chese.cpp
@@ -2,17 +2,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int countPaint(char, int, int);
 int main(void)
 {
     int M, N = 0;
     cin >> N >> M;
-    vector<string> v;
+    vector<vector<char>> v(M, vector<char>(N, 0));
+    int count = 0;
 
     for (int i = 0; i < M; i++)
     {
         string str;
         cin >> str;
-        v.push_back(str);
+        for (int j = 0; j < N; j++)
+        {
+            v[i][j] = str.charAt(j);
+        }
     }
+
     return 0;
+}
+
+int countPaint(char start, int M, int N)
+{
+    int answer = 0;
+    return answer;
 }
