cd848df99638378b8224a4e131de2b11fa32ac44
changbambi <doradoralee@naver.com>
2019-07-15 19:13:17 +0900

    #1018

--- 10.bruteforce/chese.cpp
+++ 10.bruteforce/chese.cpp
@@ -2,29 +2,78 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int countPaint(char, int, int);
+int countPaint(char, int, int, vector<vector<char>> &v);
 int main(void)
 {
     int M, N = 0;
     cin >> N >> M;
-    vector<vector<char>> v(M, vector<char>(N, 0));
-    int count = 0;
+    vector<vector<char>> v(N, vector<char>(M, 0));
+    int count = INT_MAX;
 
-    for (int i = 0; i < M; i++)
+    for (int i = 0; i < N; i++)
     {
         string str;
         cin >> str;
-        for (int j = 0; j < N; j++)
-        {
-            v[i][j] = str.charAt(j);
-        }
+        for (int j = 0; j < M; j++)
+            v[i][j] = str.at(j);
     }
+
+    for (int i = 0; i < N - 7; i++)
+    {
+        for (int j = 0; j < M - 7; j++)
+            count = min(count, countPaint(v[i][j], i, j, v));
+    }
+
+    cout << count << '\n';
 
     return 0;
 }
 
-int countPaint(char start, int M, int N)
+int countPaint(char start, int x, int y, vector<vector<char>> &v)
 {
     int answer = 0;
+    int index = 0;
+
+    if (start == 'W')
+    {
+        string check1 = "WBWBWBWB";
+        string check2 = "BWBWBWBW";
+        for (int i = x; i < x + 8; i++)
+        {
+            for (int j = y; j < y + 8; j++)
+            {
+                if (i % 2 == 0)
+                    if (check1.at(index) != v[i][j])
+                        answer++;
+                if (i % 2 == 1)
+                    if (check2.at(index) != v[i][j])
+                        answer++;
+
+                index++;
+            }
+            index = 0;
+        }
+    }
+
+    if (start == 'B')
+    {
+        string check1 = "BWBWBWBW";
+        string check2 = "WBWBWBWB";
+        for (int i = x; i < x + 8; i++)
+        {
+            for (int j = y; j < y + 8; j++)
+            {
+                if (i % 2 == 0)
+                    if (check1.at(index) != v[i][j])
+                        answer++;
+                if (i % 2 == 1)
+                    if (check2.at(index) != v[i][j])
+                        answer++;
+
+                index++;
+            }
+            index = 0;
+        }
+    }
     return answer;
 }
