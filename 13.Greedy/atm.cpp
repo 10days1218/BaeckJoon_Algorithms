96645b8a2daa281f0741e998c53a4f77cb073359
changbambi <doradoralee@naver.com>
2019-07-22 23:46:17 +0900

    #9461

--- 13.Greedy/atm.cpp
+++ 13.Greedy/atm.cpp
@@ -4,5 +4,24 @@ using namespace std;
 
 int main(void)
 {
+    int n, answer = 0;
+    cin >> n;
+    vector<int> v(n, 0);
+
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+
+    sort(v.begin(), v.end());
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            answer += v[j];
+        }
+    }
+
+    cout << answer << '\n';
+
     return 0;
 }
