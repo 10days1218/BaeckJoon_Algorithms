a8a11d3e7c7bf81ad4c2cfee021fef8af49fd9f4
changbambi <doradoralee@naver.com>
2019-07-24 22:28:41 +0900

    #9375

---
+++ 14.Math3/fashionking.py
@@ -0,0 +1,36 @@
+#9375
+
+n = int(input())
+cloth = {}
+answer = 1
+s = ''
+for i in range(0,n):
+    t = int(input())
+    for j in range(0,t):
+        str = input().split(' ')
+
+        if str[1] in cloth:
+            cloth[str[1]] +=1
+        else:
+            s = str[1]
+            cloth[s] = 1
+            
+    l = cloth.values()
+    for i in l:
+        answer = answer * ( i + 1 )
+    print(answer - 1)
+    answer = 1
+    cloth = {}
+
+
+
+
+        
+
+
+
+
+
+    
+
+    
