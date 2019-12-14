c8a704e081c13a18c697be7e73c1e2ffdaf119f4
10days1218 <doradoralee@naver.com>
2019-12-14 23:44:20 +0900

    #회문

---
+++ KOI/2019_1/palindrome.py
@@ -0,0 +1,19 @@
+T = int(input())
+strList = []
+for i in range(T):
+    strList.append(input())
+
+for i in range(len(strList))
+    ans,cnt = 0,0
+    left, right = 0, len(strList[i]) - 1
+    if strList[i][left] != strList[i][right]:
+
+    if left == right:
+        if cnt == 0:
+            print("0")
+        elif cnt == 2:
+            print("1")
+        else:
+            print("2")
+    left ,right = left + 1, right -1
+
