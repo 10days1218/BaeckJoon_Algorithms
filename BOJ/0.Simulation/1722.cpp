59a7bac4a5530f5d91c27b4b64adefe8ed1feed6
10days1218 <doradoralee@naver.com>
2020-01-15 16:43:36 +0900

    [수학] 백준#1722

--- BOJ/0.Simulation/1722.cpp
+++ BOJ/0.Simulation/1722.cpp
@@ -2,23 +2,73 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int numbers[20];
+long long fibo[21];
+bool check[21];
+int numbers[21];
 
+inline void init();
 int main(void)
 {
+    init();
+
     int n, cmd;
-    cin >> n >> cmd;
+    cin >> n;
+    cin >> cmd;
 
     if (cmd == 1)
     {
-        int k;
+        long long k;
         cin >> k;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                if (check[j] == true)
+                    continue;
+
+                if (fibo[n - i - 1] < k)
+                    k -= fibo[n - i - 1];
+
+                else
+                {
+                    check[j] = true;
+                    numbers[i] = j;
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+            cout << numbers[i] << " ";
+        cout << '\n';
     }
 
     else if (cmd == 2)
     {
+        long long ans = 1;
         for (int i = 0; i < n; i++)
             cin >> numbers[i];
+
+        for (int i = 0; i < n; i++)
+        {
+            int number = numbers[i];
+            for (int j = 1; j < number; j++)
+            {
+                if (check[j] == false)
+                    ans += fibo[n - i - 1];
+            }
+            check[number] = true;
+        }
+        cout << ans << '\n';
     }
+
     return 0;
+}
+
+inline void init()
+{
+    fibo[0] = 1, fibo[1] = 1;
+    for (int i = 2; i <= 20; i++)
+        fibo[i] = fibo[i - 1] * i;
 }
