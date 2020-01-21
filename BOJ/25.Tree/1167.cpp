62dffba9545fe6eeb4f527f10aa87a7067d63e4d
10days1218 <doradoralee@naver.com>
2020-01-21 17:40:00 +0900

    [트리,dfs]백준#1167

--- BOJ/25.Tree/1167.cpp
+++ BOJ/25.Tree/1167.cpp
@@ -2,6 +2,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<pair<int, int>> g[100001];
+bool visited[100001];
+int answer, answerNode;
+
+void dfs(int node, int cost);
 int main(void)
 {
     int n;
@@ -15,8 +20,38 @@ int main(void)
         int value[2];
         while (1)
         {
-            int
+            int c, w;
+            cin >> c;
+            if (c == -1)
+                break;
+
+            cin >> w;
+            g[node].push_back({c, w});
         }
     }
+
+    dfs(1, 0);
+    answer = 0;
+    fill(visited, visited + 100001, false);
+    dfs(answerNode, 0);
+
+    cout << answer << '\n';
     return 0;
+}
+
+void dfs(int node, int cost)
+{
+    if (visited[node])
+        return;
+
+    visited[node] = true;
+
+    if (cost > answer)
+    {
+        answer = cost;
+        answerNode = node;
+    }
+
+    for (int i = 0; i < g[node].size(); i++)
+        dfs(g[node][i].first, g[node][i].second + cost);
 }
