5647ebf045991f4394277576260eca1e6b89d8cb
10days1218 <doradoralee@naver.com>
2020-01-24 23:03:13 +0900

    [완탐]백준#1107

--- BOJ/10.bruteforce/remotecontrol.cpp
+++ BOJ/10.bruteforce/remotecontrol.cpp
@@ -2,8 +2,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int remote[10];
 int main(void)
 {
-    ;
+    int n, m;
+    cin >> n;
+    cin >> m;
+
+    for (int i = 0; i < m; i++)
+    {
+        int number;
+        cin >> number;
+        remote[number] = 1;
+    }
+
+    for (int i = 0; i <= 500000; i++)
+    {
+        ;
+    }
+
     return 0;
 }
