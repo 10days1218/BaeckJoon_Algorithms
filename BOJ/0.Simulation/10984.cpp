b57680fa83a53e7892718f8413a20a3981581580
10days1218 <doradoralee@naver.com>
2020-02-01 23:41:05 +0900

    [구현]백준#10984

---
+++ BOJ/0.Simulation/10984.cpp
@@ -0,0 +1,7 @@
+//https://www.acmicpc.net/problem/10984
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    return 0;
+}
---
+++ BOJ/15.Stack/1918.cpp
@@ -0,0 +1,16 @@
+//https://www.acmicpc.net/problem/1918
+#include <bits/stdc++.h>
+using namespace std;
+
+map<char, int> priority;
+int main(void)
+{
+    priority['+'] = 0;
+    priority['-'] = 0;
+    priority['*'] = 1;
+    priority['/'] = 1;
+    priority['('] = -1;
+    stack
+    
+    return 0;
+}
