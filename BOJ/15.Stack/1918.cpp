e680371bb3ad1197b7ec40762ade833f1612bbca
10days1218 <doradoralee@naver.com>
2020-02-04 19:13:26 +0900

    [스택]백준#1918

--- BOJ/15.Stack/1918.cpp
+++ BOJ/15.Stack/1918.cpp
@@ -2,32 +2,73 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-map<char, int> priority;
+int getPriority(char oper);
 int main(void)
 {
-    priority['+'] = 0;
-    priority['-'] = 0;
-    priority['*'] = 1;
-    priority['/'] = 1;
-    priority['('] = -1;
-    stack<char> stack;
-
     string expression;
     string answer;
+    stack<char> stack;
     cin >> expression;
 
     for (int i = 0; i < expression.size(); i++)
     {
-        if (expression[i] != '+' && expression[i] != '-' && expression[i] != '*' && expression[i] != '/' && expression[i] != '(' && expression[i] != ')')
+        if (expression[i] == '+' || expression[i] == '-' || expression[i] == '*' || expression[i] == '/' || expression[i] == '(' || expression[i] == ')')
         {
-            answer += expression[i];
+            if (stack.size() == 0)
+                stack.push(expression[i]);
+
+            else
+            {
+                if (expression[i] == '(')
+                    stack.push(expression[i]);
+
+                else if (expression[i] == ')')
+                {
+                    while (!stack.empty() && stack.top() != '(')
+                    {
+                        answer += stack.top();
+                        stack.pop();
+                    }
+                    stack.pop();
+                }
+
+                else
+                {
+                    while (!stack.empty() && getPriority(stack.top()) >= getPriority(expression[i]))
+                    {
+                        answer += stack.top();
+                        stack.pop();
+                    }
+                    stack.push(expression[i]);
+                }
+            }
         }
 
         else
-        {
-            ;
-        }
+            answer += expression[i];
     }
 
+    while (!stack.empty())
+    {
+        answer += stack.top();
+        stack.pop();
+    }
+
+    cout << answer << '\n';
+
     return 0;
+}
+
+int getPriority(char oper)
+{
+    if (oper == '+' || oper == '-')
+        return 1;
+
+    else if (oper == '*' || oper == '/')
+        return 2;
+
+    else if (oper == '(' || oper == ')')
+        return 0;
+
+    return -1;
 }
