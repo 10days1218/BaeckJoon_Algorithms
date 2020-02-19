358f70d7765ae490d59f2ae96789dd616a3a671c
10days1218 <doradoralee@naver.com>
2020-02-19 23:43:55 +0900

    백준#17281

--- samsung/1/17144.cpp
+++ samsung/1/17144.cpp
@@ -18,33 +18,48 @@ void spread();
 void clean();
 int main(void)
 {
+    int testcase;
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
 
-    cin >> R >> C >> T;
+    freopen("input.txt", "r", stdin);
+    cin >> testcase;
 
-    for (int i = 0; i < R; i++)
+    for (int t = 1; t <= testcase; t++)
     {
-        for (int j = 0; j < C; j++)
+        int answer = 0;
+        cin >> R >> C >> T;
+
+        for (int i = 0; i < R; i++)
         {
-            cin >> maps[i][j];
-            if (maps[i][j] == -1)
+            for (int j = 0; j < C; j++)
             {
-                if (cleaner.r1 == 0)
-                    cleaner.r1 = i;
-                else
-                    cleaner.r2 = i;
+                cin >> maps[i][j];
+                if (maps[i][j] == -1)
+                {
+                    if (cleaner.r1 == 0)
+                        cleaner.r1 = i;
+                    else
+                        cleaner.r2 = i;
+                }
             }
         }
-    }
 
-    for (int t = 0; t < T; t++)
-    {
-        spread();
-        clean();
+        for (int t = 0; t < T; t++)
+        {
+            spread();
+            clean();
+        }
+
+        for (int i = 0; i < R; i++)
+            for (int j = 0; j < C; j++)
+                if (maps[i][j] != -1)
+                    answer += maps[i][j];
+
+        cout << "# " << testcase << " " << answer << '\n';
+        return 0;
     }
-    return 0;
 }
 
 void spread()
@@ -82,11 +97,11 @@ void spread()
 void clean()
 {
     //↓
-    for (int i = 0; i < cleaner.r1; i++)
+    for (int i = 0; i < cleaner.r1 - 1; i++)
         maps[i + 1][0] = maps[i][0];
 
     //←
-    for (int i = 1; i < R; i++)
+    for (int i = 1; i <= R; i++)
         maps[0][i - 1] = maps[0][i];
 
     //↑
@@ -94,10 +109,26 @@ void clean()
         maps[i - 1][C - 1] = maps[i][C - 1];
 
     //→
-    for (int i = C - 1; i > 0; i++)
+    for (int i = C - 1; i > 0; i--)
         maps[cleaner.r1][i] = maps[cleaner.r1][i - 1];
 
     maps[cleaner.r1][1] = 0;
 
-    //
+    //↑
+    for (int i = cleaner.r2 + 1; i < R - 1; i++)
+        maps[i][0] = maps[i + 1][0];
+
+    //←
+    for (int i = 0; i < C - 1; i++)
+        maps[R - 1][i] = maps[R - 1][i + 1];
+
+    //↓
+    for (int i = R - 1; i > cleaner.r2; i--)
+        maps[i][C - 1] = maps[i - 1][C - 1];
+
+    //→
+    for (int i = C - 1; i > 0; i--)
+        maps[cleaner.r2][i] = maps[cleaner.r2][i - 1];
+
+    maps[cleaner.r2][1] = 0;
 }
---
+++ samsung/1/17281.cpp
@@ -0,0 +1,8 @@
+//https://www.acmicpc.net/problem/17281
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    return 0;
+}
